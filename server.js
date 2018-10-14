9d2d6467be215abfdb7ac2f5766f31a1e3193d79
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-14 12:51:32 -0500

    put the port number into a variable and used that variable in the app.listen function

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-		console.log('shits listening on port 3000');
+app.listen(PORT, () => {
+		console.log('Port 3000 works');
+		console.log('another console log bc i can');
 })
